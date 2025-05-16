cff1df43be7ae1ad454a0e0341aa91d5fb1ed60a
João Pedro <joaope.espindola2005@gmail.com>
2025-05-15 22:16:13 -0300

    Page: Home

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,5 +1,51 @@
+import { Container, Content, Heading, Info } from "./styles"
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+
 export function Home() {
  return(
-  <h1>home</h1>
+  <Container>
+   <Content>
+    <div>
+     <Heading>
+      <h1>Encontre o café perfeito para qualquer hora do dia
+      </h1>
+      <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+     </Heading>
+
+     <Info>
+      <div>
+        <div className="icon-wrapper yellow-dark">
+          <ShoppingCart weight="fill" size={16} />
+        </div>
+        <span>Compra simples e segura</span>
+      </div>
+
+       <div>
+          <div className="icon-wrapper base-text">
+            <Package weight="fill" size={16} />
+          </div>
+          <span>Entrega mantém o café intacto</span>
+        </div>
+       <div>
+        
+       <div className="icon-wrapper yellow">
+         <Timer weight="fill" size={16} />
+        </div>
+         <span>Entrega rápida e rastreada</span>
+       </div>
+
+       <div>
+         <div className="icon-wrapper purple">
+           <Coffee weight="fill" size={16} />
+         </div>
+         <span>O café chega fresquinho até você</span>
+       </div>
+     </Info>
+
+    </div>
+
+    <img src="/img/coffee.svg" alt="img de um copo flutuando no background"/>
+   </Content>
+  </Container>
  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,93 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.main`
+ max-width: 70rem;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin: 0 auto;
+`
+
+export const Content = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ padding-top: 5.87rem;
+
+ > div {
+  margin-right: 2.5rem;
+ }
+`
+
+export const Heading = styled.div`
+ display: flex;
+ flex-direction: column;
+ gap: 1rem;
+
+ h1 {
+  font-family: 'Baloo 2', sans-serif;
+  font-size: 3rem;
+  line-height: 130%;
+  font-weight: 800;
+  color: ${(props => props.theme.COLORS["base-title"])}
+ }
+
+ p {
+  font-size: 1.25rem;
+  line-height: 130%;
+  color: ${(props => props.theme.COLORS["base-subtitle"])};
+  margin-bottom: 4.12rem;
+ }
+`
+
+const baseIconStyle = css`
+  height: 32px;
+  width: 32px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+`;
+
+export const Info = styled.div`
+ display: grid;
+ grid-template-columns: 1fr 1fr;
+ gap: 1rem;
+
+ .icon-wrapper {
+  ${baseIconStyle}
+  background-color: ${(props) => props.theme.COLORS["yellow-dark"]};
+  color: ${(props) => props.theme.COLORS.background};
+ }
+ 
+ .yellow {
+  ${baseIconStyle};
+  background-color: ${(props) => props.theme.COLORS.yellow};
+  }
+
+  .yellow-dark {
+  ${baseIconStyle};
+  background-color: ${(props) => props.theme.COLORS["yellow-dark"]};
+  }
+
+  .base-text {
+  ${baseIconStyle};
+  background-color: ${(props) => props.theme.COLORS["base-text"]};
+  }
+
+  .purple {
+   ${baseIconStyle};
+   background-color: ${(props) => props.theme.COLORS.purple};
+  }
+
+ > div {
+   display: flex;
+   align-items: center;
+   gap: .8rem;
+
+   span {
+    color: ${(props) => props.theme.COLORS["base-text"]};
+   }
+  }
+`
